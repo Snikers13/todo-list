0d6e7bec9d4f1f1e5a67f499ba8c55b1818cc733
Anton Tretyak <for.anton.tretyak@gmail.com>
2018-03-27 02:08:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-let inp = document.getElementById ('myInput');
-let btn = document.getElementById ('myBtn');
-let ul = document.getElementById ('myULlist');
-let del = document.getElementById ('del');
+(function () { 
+let inp = document.getElementById('myInput');
+let btn = document.getElementById('myBtn');
+let ul = document.getElementById('myULlist');
+let del = document.getElementById('del');
 let li = document.getElementsByTagName('li');
 let byCheck = document.querySelector('ul');
 let close = document.getElementsByClassName('closeBtn');
 
 
 btn.addEventListener('click', function () {
-	let newLi = document.createElement('li');
+  let newLi = document.createElement('li');
   newLi.textContent = inp.value;
   	if (inp.value === '') {
-        let err = setInterval(function () { 
+      let err = setInterval(function () { 
         inp.placeholder = 'You must write something';
         inp.style.backgroundColor = '#FEEBE6';
           if (inp.value !== '') {
-              clearInterval(err);
+            clearInterval(err);
               inp.placeholder = 'Add what you need to do...';
               inp.style.backgroundColor = 'white';
-          }
+            }
         }, 0)
-  	} else {
+  	} else  {
     	ul.appendChild (newLi);
     }
     inp.value = '';
@@ -33,15 +34,15 @@ btn.addEventListener('click', function () {
 
 	for (let i = 0; i < close.length; i++) {
   	close[i].addEventListener('click', function() {
-    	let div = this.parentElement;
-    	let c = confirm('Do you really want delete this element?');
-    	if(c) {
-    		div.style.display = 'none';
-    	} else {
-    		span.className = 'closeBtn';
-    	}
+        let div = this.parentElement;
+        let agreeOrDisagree = confirm('Do you really want delete this element?');
+          if (agreeOrDisagree) {
+    		    div.style.display = 'none';
+          } else {
+    		    span.className = 'closeBtn';
+          }
 
-  	})
+    })
 	}
 });
 
@@ -52,8 +53,9 @@ byCheck.addEventListener('click', function (e) {
 }, false)
 
 del.addEventListener ('click', function () {
-	for (let i = 0; i < li.length; i++) {
-     		let div = li[i];
-      		div.style.display = 'none';
+		for (let i = 0; i < li.length; i++) {
+      let div = li[i];
+      div.style.display = 'none';
     }   
 })
+})();
